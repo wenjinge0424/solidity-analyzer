a3dc78982adadb78675329ea782179cb2a72c95c
WenJinGe <wenjinge0424@outlook.com>
2017-10-07 14:10:32 -0400

    Typos

--- test/inputs/input3.sol
+++ test/inputs/input3.sol
@@ -4,27 +4,27 @@ contract MyContract {
   address _tobDelegated;
 
   function g() {
-  	_tobDelegated.delegatecall(msg.data);;
+  	_tobDelegated.delegatecall(msg.data);
   }
 
   function f() {
-    _tobDelegated.delegatecall(msg.data);;
+    _tobDelegated.delegatecall(msg.data);
   }
 
   function () {
-    _tobDelegated.delegatecall(msg.data);;
+    _tobDelegated.delegatecall(msg.data);
   }
 
   function no1() {
-    _tobDelegated.delegatecall();;
+    _tobDelegated.delegatecall();
   }
 
   function no2() {
-    _tobDelegated.delegatecall(msg.data, 1);;
+    _tobDelegated.delegatecall(msg.data, 1);
   }
 
   function no3() {
-    delegatecall(msg.data);;
+    delegatecall(msg.data);
   }
 
 }
